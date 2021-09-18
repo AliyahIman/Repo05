a846aa825a39f00603b66b20f1e304f16375c7cb
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 07:01:11 +0200

    added index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,14 @@
+function main(){
+    // Only change code below this line
 
+   var  lastName = "Sabanac",
+        lastNameLength =lastName.length;
+      
+
+    // Only change code above this line
+
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main; 
