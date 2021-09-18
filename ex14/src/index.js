a010184983be7cb55bf5891535cf92b4d9d075ac
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 07:19:23 +0200

    added index.js

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,1 +1,11 @@
+function main(){
+    // Only change code below this line
+   var  myNoun= "dog", myVerb= "ran", myAdjective= "big", myAdverb= "Quickly";        
+   var  wordBlanks = myAdverb + " " + myAdjective + " " + myNoun + " " + myVerb + ".";
+    // Only change code above this line
 
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main; 
