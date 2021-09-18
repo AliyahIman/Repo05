da90f2b444f2589eed038cf40bbf4b59456ab660
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 07:05:59 +0200

    added index.js :D

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,17 @@
+function main(){
+    // Only change code below this line
 
+   var  lastName = "Sabanac",
+        firstLetterOfLastName = " ";
+
+        firstLetterOfLastName = lastName[0];
+        
+      
+
+    // Only change code above this line
+
+    return firstLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main; 
