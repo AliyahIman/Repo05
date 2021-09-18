f49e7112f0512775857cc8b298b5c56a7ea3d160
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 06:36:09 +0200

    added index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,14 @@
+function main(){
+    // Only change code below this line
+    var myFirstString= "I tink therefore I \"am\" -- sentient.",
+        mySecondString="No, you only \"think\", so you aren’t."
+    // Only change code above this line
 
+    return{
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+module.exports = main;
