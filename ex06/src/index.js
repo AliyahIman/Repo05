80f3cb540cb1ccfc92c49ea47d5456fccc8458a6
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 06:53:04 +0200

    added index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,13 @@
+function main(){
+    // Only change code below this line
 
+   var myName = "Iman";
+   var myString = "My name is " + myName + " and I am well!";
+
+    // Only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main; 
