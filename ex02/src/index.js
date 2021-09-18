d112cc28fde64b64d0ce0aac590569d622aadfb1
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-18 06:44:17 +0200

    added index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,13 @@
+function main(){
+    // Only change code below this line
+    var myLink = <a href="http://www.example.com" target="_blank">Link</a>
+    // Only change code above this line
 
+    return{
+        myFirstString,
+        mySecondString
+    };
+}
+
+console.log(main());
+module.exports = main;
